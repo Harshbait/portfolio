e96a06dc25751fe9e3626422bcc0779112540813
Harsh bait <harshbait79@gmail.com>
2025-06-11 23:30:12 +0530

    Major update

--- src/pages/page5.jsx
+++ src/pages/page5.jsx
@@ -18,7 +18,7 @@ const Page5 = () => {
       stagger: 1,
       scrollTrigger:{
         trigger: '.rotat',
-        start: 'top 137%',
+        start: 'top 147%',
         end: 'top 10%',
         scrub: 2,
         },
