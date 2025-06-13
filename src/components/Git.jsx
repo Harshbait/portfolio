46dede4128bf7348f00f87df441409300274ead7
Harsh bait <harshbait79@gmail.com>
2025-06-13 23:15:04 +0530

    Major update

--- src/components/Git.jsx
+++ src/components/Git.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Git = () => {
   return (
     <div className='absolute z-20 sm:px-3 sm:right-0 right-21.5 w-[100vw] py-[88vh] sm:py-10 flex items-center justify-center'>
-        <a href="https://github.com/Harshbait"><img className='h-10 sm:h-12' src="https://logos-world.net/wp-content/uploads/2020/11/GitHub-Symbol.png" alt="" /></a>
-        <a href="https://www.linkedin.com/in/hari-bait-82b1ab288?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"><img className='h-11 sm:h-13' src="https://img.icons8.com/?size=100&id=8808&format=png&color=000000" alt="" /></a>
+        <a href="https://github.com/Harshbait"><img className='h-10 transition-transform duration-700 ease-in-out hover:scale-120 sm:h-12' src="https://logos-world.net/wp-content/uploads/2020/11/GitHub-Symbol.png" alt="" /></a>
+        <a href="https://www.linkedin.com/in/hari-bait-82b1ab288?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"><img className='h-11 transition-transform duration-700 ease-in-out hover:scale-120 sm:h-13' src="https://img.icons8.com/?size=100&id=8808&format=png&color=000000" alt="" /></a>
       
     </div>
   )
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,7 +6,7 @@ const Projects = () => {
       <a href="https://chillis.vercel.app/" target="_blank" rel="noopener noreferrer">
         <img
           src="Chilis.png"
-          className="h-[100px] sm:h-[30vh] sm:mt-[20vh] sm:ml-[10vh] hover:scale-105 transition-transform duration-600"
+          className="h-[100px] sm:h-[30vh] sm:mt-[20vh] sm:ml-[10vh] transition-transform duration-700 ease-in-out hover:scale-105 "
           alt="Chillis Project"
         />
       </a>
@@ -14,7 +14,7 @@ const Projects = () => {
       <a href="https://chats-hliz.onrender.com/" target="_blank" rel="noopener noreferrer">
         <img
           src="Chats.png"
-          className="h-[100px] sm:h-[30vh] sm:ml-[10vh] hover:scale-105 transition-transform duration-600"
+          className="h-[100px] sm:h-[30vh] sm:ml-[10vh] ease-in-out transition-transform duration-700 hover:scale-105 "
           alt="Chats Project"
         />
       </a>
