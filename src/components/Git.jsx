fb20641672bde8036633a75b262ad6796c6f0c0c
Harsh bait <harshbait79@gmail.com>
2025-06-30 15:37:52 +0530

    Major update

--- src/components/Git.jsx
+++ src/components/Git.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Git = () => {
   return (
-    <div className='absolute z-20 sm:px-3 sm:right-0 right-21.5 w-[100vw] py-[88vh] sm:py-10 flex items-center justify-center'>
+    <div className='absolute z-30 sm:px-3 sm:right-0 w-[20vw] py-[88vh] sm:py-10 flex items-center mr-[35vw]'>
         <a href="https://github.com/Harshbait"><img className='h-10 transition-transform duration-700 ease-in-out hover:scale-120 sm:h-12' src="https://logos-world.net/wp-content/uploads/2020/11/GitHub-Symbol.png" alt="" /></a>
         <a href="https://www.linkedin.com/in/hari-bait-82b1ab288?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"><img className='h-11 transition-transform duration-700 ease-in-out hover:scale-120 sm:h-13' src="https://img.icons8.com/?size=100&id=8808&format=png&color=000000" alt="" /></a>
       
