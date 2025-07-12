be3ba18c021eafba3dc5c68ac72eee4a2e4c3aea
Harsh bait <harshbait79@gmail.com>
2025-07-12 11:24:43 +0530

    Major update

--- src/components/Myproject.jsx
+++ src/components/Myproject.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Myproject = () => {
   return (
     <div>
-       <div className='rota'><h1 className="absolute text-[60px] sm:text-[10vw] font-[Potfilo2] px-1 py-2 sm:py-5 sm:px-0 text-white">My Pojects</h1></div>
+       <div className='rota'><h1 className="absolute text-[60px] sm:text-[10vw] font-[Potfilo2] px-1 py-2 sm:py-5 sm:px-0 text-white">My Projects</h1></div>
        <h1 className='font-bold text-[20px] sm:text-[4vh] mt-23 sm:mt-[28vh]'>I have developed several projects, including:</h1>
        <h1 className='font-[Potfilo1] text-[13px] sm:text-[3vh]'>A real-time chat web application using Socket.io</h1>
        <h1 className='font-[Potfilo1] text-[13px] sm:text-[3vh]'>An animated project for <span className='font-extrabold'>ThirtysixStudios</span>, featuring animated chillis.</h1>
